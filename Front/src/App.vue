d8b812a0aeeed1aa29abcbf24c67a03a9f740109
pavel <pavelnesvel@yahoo.fr>
2025-02-21 10:33:25 +0100

    element du front

--- Front/src/App.vue
+++ Front/src/App.vue
@@ -2,7 +2,19 @@
 
 <template>
   <div>
-    <h1>HELLO WORD</h1>
+    <h1>Projet systeme embarquer</h1>
+
+    <div class="content">
+      <div>Buze <button>value</button><span> onOf</span></div>
+      <div>temperature <button>value</button></div>
+      <div>humidité <button>value</button></div>
+      <div>
+        RGB <input type="number" name="numbe1r" id="numer1" min="1" max="255" />
+        <input type="number" name="number2" id="numer2" min="1" max="255" />
+        <input type="number" name="number3" id="numer3" min="1" max="255" />
+        <span> onOf</span>
+      </div>
+    </div>
   </div>
 </template>
 
